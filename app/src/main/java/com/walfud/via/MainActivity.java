aae363860cebcf0039b4504e57eeec7917a1a67e
duanshenglian <duanshenglian@talicai.com>
2016-11-17 17:45:48 +0800

    添加ViaWebView

--- app/src/main/java/com/walfud/via/MainActivity.java
+++ app/src/main/java/com/walfud/via/MainActivity.java
@@ -2,6 +2,7 @@ package com.walfud.via;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView webView = (WebView) findViewById(R.id.wv);
+        webView.loadUrl("file:///android_asset/test.html");
     }
 }
